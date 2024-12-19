18da6671f9ade8ba74de4a30dc68d114040965fb
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-12-19 21:08:54 +0300

    add basic data type

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,15 @@
 import './style.css'
+
+function displayMessage(message: string) {
+  const appDiv = document.getElementById('app');
+  if (appDiv) {
+    const messageParagraph = document.createElement('p');
+    appDiv.appendChild(messageParagraph);
+    messageParagraph.textContent = message;
+  }
+}
+
+// Basic Data Types
+let firstName: string = 'Edward';
+let greeting: string = `Hi there! ${firstName} God loves you`;
+displayMessage(greeting)
