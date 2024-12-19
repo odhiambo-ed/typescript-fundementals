67f583cfe4ed038b600810bb64090d12eac7a9bd
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-12-19 22:36:32 +0300

    add app div to ts

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,21 @@
 import './style.css'
 
-function displayMessage(message: string) {
-  const appDiv = document.getElementById('app');
-  if (appDiv) {
-    const messageParagraph = document.createElement('p');
-    appDiv.appendChild(messageParagraph);
-    messageParagraph.textContent = message;
+function displayMessage(result: string, comment: string, container: string) {
+  const containerDiv = document.getElementById('container__app');
+  if (containerDiv) {
+    const appDiv = document.createElement(`${container}`);
+    appDiv.setAttribute('id', 'app');
+    containerDiv.appendChild(appDiv);
+    const messageParagraphOne = document.createElement('p');
+    const messageParagraphTwo = document.createElement('p');
+    appDiv.appendChild(messageParagraphOne);
+    appDiv.appendChild(messageParagraphTwo);
+    messageParagraphOne.textContent = `// ${comment}`;
+    messageParagraphTwo.textContent = result;
   }
 }
 
 // Basic Data Types
 let firstName: string = 'Edward';
 let greeting: string = `Hi there! ${firstName} God loves you`;
-displayMessage(greeting)
+displayMessage(greeting, 'String', 'div')
